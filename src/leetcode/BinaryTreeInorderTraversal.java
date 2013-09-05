3b646468ea9ef7f3817ee9b4306093c64ebbec58
wanhao <wanhao1208@gmail.com>
2013-09-06 01:13:06 +0800

    Binary Tree Inorder Traversal

---
+++ src/leetcode/BinaryTreeInorderTraversal.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.ArrayList;
+
+public class BinaryTreeInorderTraversal {
+
+	public static void main(String[] args) {
+		
+	}
+	
+	public ArrayList<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> result=new ArrayList<Integer>();
+        if(root==null) return result;
+        
+        
+    }
+	
+	static class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+}
