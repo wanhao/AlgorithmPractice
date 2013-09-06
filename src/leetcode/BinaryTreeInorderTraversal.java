dfd7c767a425f29b771f77184387d37610337e17
wanhao <wanhao1208@gmail.com>
2013-09-06 08:57:05 +0800

    Solve Binary Tree Inorder Traversal

--- src/leetcode/BinaryTreeInorderTraversal.java
+++ src/leetcode/BinaryTreeInorderTraversal.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class BinaryTreeInorderTraversal {
 
@@ -12,7 +13,20 @@ public class BinaryTreeInorderTraversal {
         ArrayList<Integer> result=new ArrayList<Integer>();
         if(root==null) return result;
         
+        Stack<TreeNode> stack=new Stack<TreeNode>();
+
+        while(root!=null||!stack.isEmpty()){
+        	if(root!=null){
+        		stack.push(root);
+        		root=root.left;
+        	}else{
+        		root=stack.pop();
+        		result.add(root.val);
+        		root=root.right;
+        	}
+        }
         
+        return result;
     }
 	
 	static class TreeNode {
