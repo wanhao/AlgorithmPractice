34bdd776f2fb9e2906a0e229355a0183aedb0251
wanhao <wanhao1208@gmail.com>
2013-09-11 21:29:46 +0800

    Update Restore IP Addresses

--- src/leetcode/RestoreIPAddresses.java
+++ src/leetcode/RestoreIPAddresses.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 
 public class RestoreIPAddresses {
-
+	
 	public static void main(String[] args) {
 		RestoreIPAddresses t=new RestoreIPAddresses();
 		
@@ -12,46 +12,44 @@ public class RestoreIPAddresses {
 	}
 
 	public ArrayList<String> restoreIpAddresses(String s) {
-		HashSet<String> result=new HashSet<String>();
-		restore(s,0,4,new ArrayList<Integer>(),result);
-		ArrayList<String> finalResult=new ArrayList<String>();
-		finalResult.addAll(result);
-		return finalResult;
+		ArrayList<String> result=new ArrayList<String>();
+		restore(s,0,4,new ArrayList<String>(),result);
+		return result;
 	}
 
 	private boolean restore(String s, int pos, int remain,
-			ArrayList<Integer> posList, HashSet<String> resultList) {
+			ArrayList<String> posList, ArrayList<String> resultList) {
 		if((s.length()-pos)>3*remain ||(s.length()-pos)<remain) return false;
 		if(remain<=0){
 			if(pos!=s.length()) return false;
 			
 			StringBuilder sb=new StringBuilder();
 			for(int i=0;i<3;i++){
-				sb.append(Integer.valueOf(s.substring(posList.get(i),posList.get(i+1))));
+				sb.append(posList.get(i));
 				sb.append('.');
 			}
-			sb.append(Integer.valueOf(s.substring(posList.get(3),s.length())));
+			sb.append(posList.get(3));
 			resultList.add(sb.toString());
 			
 			return true;
 		}else{
 			boolean valid=false;
-			if(s.length()-pos>=1 && Integer.valueOf(s.substring(pos,pos+1))<=255){
-				posList.add(pos);
+			if(s.length()-pos>=1){
+				posList.add(s.substring(pos,pos+1));
 				if(restore(s,pos+1,remain-1,posList,resultList)){
 					valid=true;
 				}
 				posList.remove(4-remain);
 			}
-			if(s.length()-pos>=2 && Integer.valueOf(s.substring(pos,pos+2))<=255){
-				posList.add(pos);
+			if(s.length()-pos>=2 && s.charAt(pos)!='0'){
+				posList.add(s.substring(pos,pos+2));
 				if(restore(s,pos+2,remain-1,posList,resultList)){
 					valid=true;
 				}
 				posList.remove(4-remain);
 			}
-			if(s.length()-pos>=3 && Integer.valueOf(s.substring(pos,pos+3))<=255){
-				posList.add(pos);
+			if(s.length()-pos>=3  && s.charAt(pos)!='0'&& Integer.valueOf(s.substring(pos,pos+3))<=255){
+				posList.add(s.substring(pos,pos+3));
 				if(restore(s,pos+3,remain-1,posList,resultList)){
 					valid=true;
 				}
