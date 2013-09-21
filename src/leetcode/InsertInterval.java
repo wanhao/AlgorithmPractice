4c8f43d78ba9c579252e8bd0d75e4d13c2698575
wanhao <wanhao1208@gmail.com>
2013-09-21 12:14:33 +0800

    update Insert Interval

--- src/leetcode/InsertInterval.java
+++ src/leetcode/InsertInterval.java
@@ -8,12 +8,13 @@ public class InsertInterval {
 	public static void main(String[] args) {
 		InsertInterval t = new InsertInterval();
 		ArrayList<Interval> result = new ArrayList<Interval>();
-		result.add(new Interval(1, 5));
-//		result.add(new Interval(2, 6));
-//		result.add(new Interval(8, 10));
-//		result.add(new Interval(15, 18));
+		result.add(new Interval(1, 2));
+		result.add(new Interval(3, 5));
+		result.add(new Interval(6, 7));
+		result.add(new Interval(8, 10));
+		result.add(new Interval(12, 16));
 
-		System.out.println(t.insert(result, new Interval(6,8)));
+		System.out.println(t.insert(result, new Interval(4,9)));
 	}
 
 	public ArrayList<Interval> insert(ArrayList<Interval> intervals,
@@ -24,37 +25,65 @@ public class InsertInterval {
 		}
 
 		ArrayList<Interval> result = new ArrayList<Interval>();
-		if(newInterval.end<intervals.get(0).start){
-			result.add(newInterval);
-			result.addAll(intervals);
-		}else if(newInterval.start>intervals.get(intervals.size()-1).end){
-			result.addAll(intervals);
-			result.add(newInterval);
-		}else{
-			boolean found = false;
-			boolean insert= false;
-			for (int i = 0; i < intervals.size(); i++) {
-				Interval interval = intervals.get(i);
-				if (!insert && interval.start <= newInterval.end
-						&& newInterval.start <= interval.end) {
-					found = true;
-					newInterval.start = Math.min(newInterval.start, interval.start);
-					newInterval.end = Math.max(newInterval.end, interval.end);
-				}else{
-					if(found&& !insert){
-						result.add(newInterval);
-						insert=true;
-					}
-					
-					result.add(interval);
-				}
-			}
-			
-			if(!insert){
-				result.add(newInterval);
+		int startPos=-1, endPos=-1;
+		
+		Interval tmp=null;
+		int low=0,high=intervals.size()-1,middle=0;
+		while(low<=high){
+			middle=(low+high)/2;
+			tmp=intervals.get(middle);
+			if(tmp.end==newInterval.start){
+				startPos=middle;
+				break;
+			}else if(tmp.end<newInterval.start){
+				low=middle+1;
+			}else{
+				high=middle-1;
 			}
 		}
+		if(startPos<0){
+			startPos=low;
+		}
+
+		low=0;
+		high=intervals.size()-1;
+		while(low<=high){
+			middle=(low+high)/2;
+			tmp=intervals.get(middle);
+			if(tmp.start==newInterval.end){
+				endPos=middle;
+				break;
+			}else if(tmp.start<newInterval.end){
+				low=middle+1;
+			}else{
+				high=middle-1;
+			}
+		}
+		if(endPos<0){
+			endPos=high;
+		}
 		
+		if(startPos>endPos){
+			for(int i=0;i<=endPos;i++){
+				result.add(intervals.get(i));
+			}
+			result.add(newInterval);
+			for(int i=startPos;i<intervals.size();i++){
+				result.add(intervals.get(i));
+			}
+		}else{
+			for(int i=0;i<startPos && i<intervals.size();i++){
+				result.add(intervals.get(i));
+			}
+			for(int i=startPos;i<=endPos;i++){
+				newInterval.start=Math.min(newInterval.start,intervals.get(i).start);
+				newInterval.end=Math.max(newInterval.end,intervals.get(i).end);
+			}
+			result.add(newInterval);
+			for(int i=endPos+1;i<intervals.size();i++){
+				result.add(intervals.get(i));
+			}
+		}
 		
 		return result;
 	}
